ba05c673e7a67f304c36581a3264dde95cabd7c7
Justin Williams <williams.jman@gmail.com>
2022-10-27 13:41:15 -0400

    Button adds 25 buttons on click

---
+++ index.js
@@ -0,0 +1,13 @@
+const startGame = (event) => {
+  let btnDiv = document.querySelector("#buttons");
+  for (let i = 0; i <= 25; i++) {
+    let btn = document.createElement("button");
+    btn.innerText = randomNumber(1, 25);
+    btnDiv.appendChild(btn);
+  }
+};
+document.querySelector("#start").addEventListener("click", startGame);
+
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
