965b8f7df819971899ba3c8559e0db1cb5c1a755
Justin Williams <williams.jman@gmail.com>
2022-10-28 17:25:12 -0400

    updated radom value function

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 let called = false;
 const startGame = (event) => {
   let btnDiv = document.querySelector("#buttons");
+
   if (!called) {
-    for (let i = 0; i <= 25; i++) {
+    for (let i = 0; i < 25; i++) {
       let btn = document.createElement("button");
-      btn.innerText = randomNumber(1, 25);
+      btn.innerText = generateNumber(25);
       btn.setAttribute("class", "tile");
       btn.setAttribute("id", btn.innerText);
-
       btnDiv.appendChild(btn);
       called = true;
     }
   } else if (called === true) {
-    for (let i = 0; i <= 25; i++) {
+    numberArray = [];
+    for (let i = 0; i < 25; i++) {
       let btnValue = btnDiv.getElementsByTagName("button")[i];
-      btnValue.innerText = randomNumber(1, 25);
+      btnValue.innerText = generateNumber(25);
     }
   }
 };
 document.querySelector("#start").addEventListener("click", startGame);
-
-const randomNumber = (min, max) => {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
 
 const checkOrder = (event) => {
   let btnDiv = document.querySelector("#buttons");
@@ -37,3 +34,16 @@ const checkOrder = (event) => {
   }
 };
 document.querySelector("#buttons").addEventListener("click", checkOrder);
+
+let numberArray = [];
+function generateNumber(maxNumber) {
+  let randomNumber = Math.floor(Math.random() * maxNumber) + 1;
+  if (!numberArray.includes(randomNumber)) {
+    numberArray.push(randomNumber);
+    return randomNumber;
+  } else {
+    if (numberArray.length < maxNumber) {
+      return generateNumber(maxNumber);
+    }
+  }
+}
