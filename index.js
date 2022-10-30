f31825d61b2fbf6ea0df10937e96000d39b98337
Adrienne W <adrienne.whetsell@gmail.com>
2022-10-30 08:51:23 -0400

    add jsDoc comments

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+/**
+ * Starts the game by creating a grid of shuffled numbers from 1 to 25. Subsequent clicks resets the game and shuffle numbers.
+ * Adds event listener.
+ * @param {event} event Mouse Click
+ */
+
 let called = false;
 
 const startGame = (event) => {
   let btnDiv = document.querySelector("#buttons");
 
-  if (!called) {
+  if (!called) {      //First click begins game.
     for (let i = 0; i < 25; i++) {
       let btn = document.createElement("button");
       btn.innerText = generateNumber(25);
@@ -12,7 +18,7 @@ const startGame = (event) => {
       btnDiv.appendChild(btn);
       called = true;
     }
-  } else if (called === true) {
+  } else if (called === true) {    //Subsequent clicks resets the game.
     numberArray = [];
     currentAnswer = 1;
     for (let i = 0; i < 25; i++) {
@@ -24,23 +30,30 @@ const startGame = (event) => {
 };
 document.querySelector("#start").addEventListener("click", startGame);
 
+/**
+ * Checks value of clicked button against current answer. Adds event listener.
+ * @param {event} event Mouse Click
+ */
+
 let currentAnswer = 1;
 const checkOrder = (event) => {
-  let btnDiv = document.querySelector("#buttons");
   let btn = event.target.getAttribute("id");
-  console.log(currentAnswer);
 
-  if (btn == currentAnswer && btn <= 24) {
-    console.log("correct");
+  if (btn == currentAnswer && btn <= 24) {    //Button background color changes green if button clicked matches current answer and button value is <= 24.
     document.getElementById(btn).setAttribute("id", "correct");
     currentAnswer += 1;
-  } else if (currentAnswer == 25) {
-    console.log("YOU WIN");
+  } else if (currentAnswer == 25) {     //Determines when the player wins.
     document.getElementById(btn).setAttribute("id", "correct");
     document.getElementById("win").innerHTML = "YOU WIN!";
   }
 };
 document.querySelector("#buttons").addEventListener("click", checkOrder);
+
+/**
+ * Produces integers from 1 to 25 in random order.
+ * @param {maxNumber} number - Maximum number is 25.
+ * @returns {number} Returns integers in random order.
+ */
 
 let numberArray = [];
 function generateNumber(maxNumber) {
